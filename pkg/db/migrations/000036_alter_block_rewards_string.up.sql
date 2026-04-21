c8badf07f9537791df05dcca68f18b7104322bad
BitWonka <79578887+BitWonka@users.noreply.github.com>
2026-04-21 23:55:41 +0200

    fix: block rewards overflow

---
+++ pkg/db/migrations/000036_alter_block_rewards_string.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE t_block_rewards MODIFY COLUMN f_reward_fees String DEFAULT '';
+ALTER TABLE t_block_rewards MODIFY COLUMN f_burnt_fees String DEFAULT '';
+ALTER TABLE t_block_rewards MODIFY COLUMN f_bid_commission String DEFAULT '';
